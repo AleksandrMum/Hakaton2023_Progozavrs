c17b3a19b0d6b4779ee373aadbe138b5b1d30c7e
alex_mumladze <sasha.mumladze2006@gmail.com>
2023-10-06 23:12:26 +0300

    Хз работает или нет, промежуточный сейв

--- functions.py
+++ functions.py
@@ -3,21 +3,56 @@ class Telegram:
         from telebot import types
         self.types = types
 
-    def create_keyboard(self):
+    def create_keyboard_greeting(self):
         markup = self.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         buttons = self.types.KeyboardButton
-        item1 = buttons("/Показания")
+        item1 = buttons("/Список_датчиков")
         item2 = buttons("/Информация")
         markup.add(item1, item2)
         return markup
 
 
+class Requests:
+    def __init__(self):
+        import requests
+        self.requests = requests
+        self.address = "https://сайт"
+
+    def get_list_sensors(self):
+        json_sensors = self.requests.get(f"{self.address}/list_of_sensors").json()
+        number_of_sensors = int(json_sensors["number_of_sensors"])
+        list_of_sensors = json_sensors["sensors"]
+        names_of_sensors_good = []
+        names_of_sensors_bad = []
+        for number in range(number_of_sensors):
+            if list_of_sensors[number][1] == "ok":
+                names_of_sensors_good.append(list_of_sensors[number][0])
+            else:
+                names_of_sensors_bad.append(list_of_sensors[number][0])
+        str_names_of_sensors_good = '\n'.join(names_of_sensors_good)
+        str_names_of_sensors_bad = '\n'.join(names_of_sensors_bad)
+        message = (f"Список активных датчиков: \n"
+                   f"{str_names_of_sensors_good} \n\n"
+                   f"Список неактивных датчиков: \n"
+                   f"{str_names_of_sensors_bad} \n\n"
+                   f"Чтобы получить данные с активного датчика, введите '/Данные ИмяСенсора'")
+        return message
+
+    def get_sensor_data(self, name):
+        json_data = self.requests.get(f"{self.address}/{name}").json()
+        data = []
+        for item in json_data["fields"]:
+            data.append(f"{item} - {json_data[item]}")
+        message = "\n".join(data)
+        return message
+
+
 greeting = (f"Здравствуйте! Спасибо за то, что воспользовались нашим Телеграм ботом погоды.\n\n"
-            f"Чтобы получить текущие погодные данные с датчиков, нажмите кнопку 'Показания'. \n\n"
+            f"Чтобы отобразить список датчиков, нажмите кнопку 'Список_датчиков'. \n\n"
             f"Чтобы узнать больше о боте, нажмите кнопку 'Информация'")
 information1 = (f"Этот бот был разработан командой 'Прогозавры' в рамках хакатона 'Cyber Garden 2023' \n\n"
                 f"Команда состоит из трех человек: \n"
                 f"1. Дутов Илья Валерьевич - командир команды, ответственен за железо, \n"
                 f"2. Чидлеев Дмитрий Сергеевич - ответственный за сайт и серверную часть,\n"
                 f"3. Мумладзе Александр Сергоевич - ответственный за этого бота.\n\n")
-information2 = f"Чтобы получить данные с датчиков, нажмите кнопку 'Получить данные'"
+main_info = f"Чтобы отобразить список датчиков, нажмите кнопку 'Список_датчиков'"
--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import telebot
 import functions
 Telegram = functions.Telegram()
+Requests = functions.Requests()
+
 bot = telebot.TeleBot('6359375478:AAFF2M0TBQd-6Yj4TjnJS1GxT-3nh7eXczE')
-bot_keyboard = Telegram.create_keyboard()
+keyboard_greeting = Telegram.create_keyboard_greeting()
 
 
 @bot.message_handler(commands=["start"])
 def greeting(message):
-    bot.send_message(message.chat.id, functions.greeting, reply_markup=bot_keyboard)
-
-
-@bot.message_handler(commands=["Показания"])
-def send_data(message):
-    bot.send_message(message.chat.id, "Данных нет но вы держитесь")
+    bot.send_message(message.chat.id, functions.greeting, reply_markup=keyboard_greeting)
 
 
 @bot.message_handler(commands=["Информация"])
-def send_data(message):
+def information(message):
     bot.send_message(message.chat.id, functions.information1)
-    bot.send_message(message.chat.id, functions.information2)
+    bot.send_message(message.chat.id, functions.main_info)
+
+
+@bot.message_handler(commands=["Назад"])
+def back_to_main(message):
+    bot.send_message(message.chat.id, functions.main_info)
+
+
+@bot.message_handler(commands=["Список_датчиков"])
+def list_of_sensors(message):
+    send_message = Requests.get_list_sensors()
+    bot.send_message(message.chat.id, send_message)
+
+
+@bot.message_handler(content_types=["Данные"])
+def list_of_data(message):
+    input_text = message.text.replace("/Данные ", "")
+    data = Requests.get_sensor_data(input_text)
+    bot.send_message(message.chat.id, data)
 
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):  # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, "Зачем ты сюда что-то пишешь?")
+def another_text(message):  # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, "Ошибка, повторите ввод")
 
 
 print("Бот заработал")
